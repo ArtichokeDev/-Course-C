0269d0b8d85d51c70ca1221219daaa181061357c
Artichoke Dev <108453645+ArtichokeDev@users.noreply.github.com>
2022-07-01 19:42:35 +0200

    Create Ejercicio 4 - operadores assignacion.c

---
+++ Tutorials/1-Introduccion Basica/Ejercicio 4 - operadores assignacion.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // Libreria
+
+int main(){
+    int a,b,c;
+    a=10;
+    //a+=10;//a=a+10
+    //a-=5;//a=a-5
+    //a*=2;//a=a*2
+
+    a/=2;//a=a/2
+
+    printf("El valor deaes:%i",a);
+    
+    return 0;
+}
