f0c18eb4ba261d27aeeb81194820081e07a2bb15
Artichoke Dev <108453645+ArtichokeDev@users.noreply.github.com>
2022-07-01 19:31:22 +0200

    Create Ejercicio 1 - tipos datos.c

---
+++ Tutorials/1-Introduccion Basica/Ejercicio 1 - tipos datos.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // Libreria
+
+#define PI 3.1416 // Macro
+
+int y = 5; // Variable Global
+int main(){
+    int x = 10; // Variable Local
+    float suma = 0;
+    suma= PI + x;
+    printf("La suma es:%.3f",suma); //3 decimales
+    return 0;
+
+}
